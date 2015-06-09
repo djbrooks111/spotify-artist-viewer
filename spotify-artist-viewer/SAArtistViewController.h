ffcd95c19b2db7489617f32c7d69c3db3c67360b
David Brooks <davidbrooks@Davids-MacBook-Pro.local>
2015-06-09 13:41:06 -0400

    Added needed ViewControllers as according to the instructions

---
+++ spotify-artist-viewer/SAArtistViewController.h
@@ -0,0 +1,17 @@
+//
+//  SAArtistViewController.h
+//  spotify-artist-viewer
+//
+//  Created by David Brooks on 6/9/15.
+//  Copyright (c) 2015 Intrepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SAArtist;
+
+@interface SAArtistViewController : UIViewController
+
+-(instancetype)initWithArtist:(SAArtist *)artist;
+
+@end
